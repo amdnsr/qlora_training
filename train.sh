d3e1c8d0289e9c14aa98c7191774808179cd017f
Ahmad <f20190088@pilani.bits-pilani.ac.in>
2024-01-23 19:45:29 +0800

    add training script for any dataset

---
+++ train.sh
@@ -0,0 +1,1 @@
+python train_individual_model_for_individual_dataset.py --dataset_name=reddit --dataset_home=create_dataset_jsons_aadish --model_name=llama --model_adapter_output_home=model_adapter_output_home
